9c6b6d1f1042483ca8cc2883772602ca23e0bcb0
razvan <razvan3105@yahoo.com>
2022-03-14 22:51:34 +0200

    basic elements for developing FC

---
+++ src/main/java/com/example/amon/model/KnowledgeBase.java
@@ -0,0 +1,8 @@
+package com.example.amon.model;
+
+import java.util.List;
+
+public class KnowledgeBase {
+    private List<Rule> rules;
+
+}
---
+++ src/main/java/com/example/amon/model/Rule.java
@@ -0,0 +1,13 @@
+package com.example.amon.model;
+
+
+import lombok.*;
+
+import java.util.List;
+
+public class Rule {
+
+    private List<String> premises;
+    private String head;
+
+}
