c615c5952294d6f4521846989201fac949281b27
razvan <razvan3105@yahoo.com>
2022-03-20 12:05:00 +0200

    initialise premises state for FC

--- src/main/java/com/example/amon/model/KnowledgeBase.java
+++ src/main/java/com/example/amon/model/KnowledgeBase.java
@@ -1,14 +1,20 @@
 package com.example.amon.model;
 
+import lombok.Getter;
 import lombok.ToString;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@ToString
-public class KnowledgeBase {
-    private List<Rule> rules;
 
-    public void addRule(Rule rule) {
+public class KnowledgeBase {
+    private static List<Rule> rules = new ArrayList<>();
+
+    public static void addRule(Rule rule) {
         rules.add(rule);
+    }
+
+    public static List<Rule> getRules() {
+        return rules;
     }
 }
