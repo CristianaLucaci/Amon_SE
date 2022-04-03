3379f0ce8836ba95364a2bb2a2d015a7433a861c
lucacc <cristiana.lucaci@student.upt.ro>
2022-04-03 22:23:00 +0300

    Switching Scenes, get help button

--- src/main/java/com/example/amon/HelloController.java
+++ src/main/java/com/example/amon/HelloController.java
@@ -1,26 +1,34 @@
 package com.example.amon;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
-//    @FXML
-//    private Label welcomeText;
-//
-//    @FXML
-//    protected void onHelloButtonClick() {
-//        welcomeText.setText("Welcome to JavaFX Application!");
-//    }
 
-    public Button getHelp;
+    @FXML
+    private Button getHelp;
 
-    public void handleGetHelpClick(){
-        System.out.println("It worked!");
+    @FXML
+    public void handleGetHelpClick(ActionEvent event) throws IOException {
+
+        Parent formParent = FXMLLoader.load(getClass().getResource("options.fxml"));
+        Scene formScene = new Scene(formParent);
+
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(formScene);
+        window.show();
     }
 
     @Override
